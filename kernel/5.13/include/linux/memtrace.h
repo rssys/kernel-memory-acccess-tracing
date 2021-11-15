1d192d1220fa30b8519fcb4a3a63352a62378f94
Congyu Liu <liucy1998@outlook.com>
2021-11-15 17:04:21 -0500

    kernel: memory trace bug fix
    - in scalar memory access tracing, add `LOAD_VALUE` macro;
    - in memory region access, handle the case when address alignment
    is larger than length.

--- kernel/5.13/include/linux/memtrace.h
+++ kernel/5.13/include/linux/memtrace.h
@@ -28,7 +28,7 @@ static __always_inline unsigned long read_rsp(void) {
 #define LOAD_VALUE_1(dst, addr) LOAD_VALUE_TYPE(dst, addr, uint8_t)
 #define LOAD_VALUE(dst, addr, len) LOAD_VALUE_##len(dst, addr)
 
-#define __memtrace_saveip                                                                      \
+#define __memtrace_saveip    \         
 	if (p->saved_ip_len < MEMTRACE_SAVEDIP_LEN) { \
 		p->saved_ip[p->saved_ip_len] = _RET_IP_; \
 	} \
@@ -50,6 +50,7 @@ static __always_inline unsigned long read_rsp(void) {
 	unsigned long ip;\
 	__memtrace_loadip(load) \
 	int pkt_field_idx = p->mem_access_cnt&0xf;                  \
+	LOAD_VALUE(p->cur->val[pkt_field_idx], addr, len);          \
 	p->cur->pc[pkt_field_idx] = ip; \
 	p->cur->addr[pkt_field_idx] = addr;                         \
 	p->cur->info[pkt_field_idx] = load;                         \
@@ -62,11 +63,13 @@ static __always_inline unsigned long read_rsp(void) {
 #define __memtraceN(len, load)                                      \
 	unsigned long n, ip;\
 	__memtrace_loadip(load) \
-	while ((p->mem_access_cnt < MEMTRACE_NUM_MAX) && (len > 0)) {       \
+	while ((p->mem_access_cnt < MEMTRACE_NUM_MAX) && (len != 0)) {       \
 		int pkt_field_idx = p->mem_access_cnt&0xf;                  \
 		uint8_t addr_low = addr & 0xf; \
-		switch ((addr_low & (addr_low-1)) ^ addr_low) {  \
-			case 0:\
+		uint8_t aligned = (addr_low & (addr_low-1)) ^ addr_low; \
+		aligned = (aligned == 0 ? 16 : aligned) > len ? len : aligned; \
+		switch (aligned) {  \
+			case 16:\
 				LOAD_VALUE_16(p->cur->val[pkt_field_idx], addr); \
 				n = 16; \
 				break; \
